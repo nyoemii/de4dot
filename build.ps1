e8a9c1560dc7f76f524d0079465f6906b20fd4fd
Andrii Kurdiumov <kant2002@gmail.com>
2024-08-14 09:05:45 +0500

    Update supported runtimes. Mostly to do not care about Windows 7 and earler, and simplify build. If somebody showup and state their usage, I will reconsider.

--- build.ps1
+++ build.ps1
@@ -1,21 +1,9 @@
 $ErrorActionPreference = 'Stop'
 
-#
-# dotnet build isn't used because it can't build net35 tfms
-#
-
-msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net35 de4dot.netframework.sln
+dotnet build -c Release -f net48 de4dot.netframework.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item Release\net35\*.pdb, Release\net35\*.xml, Release\net35\Test.Rename.*
+Remove-Item Release\net48\*.pdb, Release\net48\*.xml, Release\net48\Test.Rename.*
 
-msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net45 de4dot.netframework.sln
+dotnet publish -c Release -f net8.0 -o publish-net8.0 de4dot
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item Release\net45\*.pdb, Release\net45\*.xml, Release\net45\Test.Rename.*
-
-dotnet publish -c Release -f netcoreapp2.1 -o publish-netcoreapp2.1 de4dot
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item publish-netcoreapp2.1\*.pdb, publish-netcoreapp2.1\*.xml
-
-dotnet publish -c Release -f netcoreapp3.1 -o publish-netcoreapp3.1 de4dot
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item publish-netcoreapp3.1\*.pdb, publish-netcoreapp3.1\*.xml
+Remove-Item publish-net8.0\*.pdb, publish-net8.0\*.xml
