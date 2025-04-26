6d0629a6f9b899bbc46531af674196f9abd14c25
Andrii Kurdiumov <kant2002@gmail.com>
2025-04-26 18:21:33 +0500

    Add tests to the CI process (#5)
    * Add tests to the CI process
    * Add path to .NET x64 framework for ILASM invocation
    * Fix temp path creation

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ $Configuration="Release"
 
 mkdir obj -Force | Out-Null
 # C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64
-$env:PATH="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64;$env:PATH"
+$env:PATH="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64;c:\Windows\Microsoft.NET\Framework64\v4.0.30319\;$env:PATH"
 
 $de4dotExec="Release\net8.0\win-x64\de4dot.exe"
 
@@ -11,7 +11,7 @@ function Test($item)
 	# tests\samples\inlining\inline_static_method_new.il
 	$base = [System.IO.Path]::GetFileNameWithoutExtension($item)
 	$dir = Split-Path $item
-	mkdir -Force $dir | Out-Null
+	mkdir -Force obj\$dir | Out-Null
 	ilasm /DLL /QUIET "$item" /OUTPUT=obj\$dir\$base.dll
 
 	& $de4dotExec  obj\$dir\$base.dll -o obj\$dir\$base.cleaned.dll
